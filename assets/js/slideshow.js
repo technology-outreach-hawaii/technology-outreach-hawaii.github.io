6592d7a3112f16f50916efb921281b02bff18b45
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-05-05 19:52:48 -1000

    Home page: update slideshow w/ dots

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -10,6 +10,10 @@ const row3 = document.getElementById("row3");
 const prev3 = document.getElementById("prev3");
 const next3 = document.getElementById("next3");
 
+const dot1 = document.getElementById("dot1");
+const dot2 = document.getElementById("dot2");
+const dot3 = document.getElementById("dot3");
+
 //arive at row1
 next3.addEventListener("click", function() {
   search("row1");
@@ -20,7 +24,6 @@ prev2.addEventListener("click", function() {
 
 // arrive at row2
 next1.addEventListener("click", function() {
-  console.log('s');
   search("row2");
 });
 prev3.addEventListener("click", function() {
@@ -41,16 +44,25 @@ function search(row) {
       row1.style.display = "flex";
       row2.style.display = "none";
       row3.style.display = "none";
+      dot1.style.opacity = "100%";
+      dot2.style.opacity = "50%";
+      dot3.style.opacity = "50%";
       break;
     case "row2":
     row1.style.display = "none";
     row2.style.display = "flex";
     row3.style.display = "none";
+    dot1.style.opacity = "50%";
+    dot2.style.opacity = "100%";
+    dot3.style.opacity = "50%";
       break;
     case "row3":
     row1.style.display = "none";
     row2.style.display = "none";
     row3.style.display = "flex";
+    dot1.style.opacity = "50%";
+    dot2.style.opacity = "50%";
+    dot3.style.opacity = "100%";
       break;
     default:
       console.log("no work");
