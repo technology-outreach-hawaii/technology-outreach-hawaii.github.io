3b425133fe559b108b2003bf65eb72253c567d7b
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-08-06 13:32:43 -1000

    Home Page: Reimplement Team Section

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -10,12 +10,17 @@ const row3 = document.getElementById("row3");
 const prev3 = document.getElementById("prev3");
 const next3 = document.getElementById("next3");
 
+const row4 = document.getElementById("row4");
+const prev4 = document.getElementById("prev4");
+const next4 = document.getElementById("next4");
+
 const dot1 = document.getElementById("dot1");
 const dot2 = document.getElementById("dot2");
 const dot3 = document.getElementById("dot3");
+const dot4 = document.getElementById("dot4");
 
 //arive at row1
-next3.addEventListener("click", function() {
+next4.addEventListener("click", function() {
   search("row1");
 });
 prev2.addEventListener("click", function() {
@@ -34,8 +39,16 @@ prev3.addEventListener("click", function() {
 next2.addEventListener("click", function() {
   search("row3");
 });
-prev1.addEventListener("click", function() {
+prev4.addEventListener("click", function() {
   search("row3");
+});
+
+//arrive at row4
+next3.addEventListener("click", function() {
+  search("row4");
+});
+prev1.addEventListener("click", function() {
+  search("row4");
 });
 
 function search(row) {
@@ -44,27 +57,45 @@ function search(row) {
       row1.style.display = "flex";
       row2.style.display = "none";
       row3.style.display = "none";
+      row4.style.display = "none";
       dot1.style.opacity = "100%";
       dot2.style.opacity = "50%";
       dot3.style.opacity = "50%";
+      dot4.style.opacity = "50%";
       break;
     case "row2":
     row1.style.display = "none";
     row2.style.display = "flex";
     row3.style.display = "none";
+    row4.style.display = "none";
     dot1.style.opacity = "50%";
     dot2.style.opacity = "100%";
     dot3.style.opacity = "50%";
+    dot4.style.opacity = "50%";
       break;
     case "row3":
     row1.style.display = "none";
     row2.style.display = "none";
     row3.style.display = "flex";
+    row4.style.display = "none";
     dot1.style.opacity = "50%";
     dot2.style.opacity = "50%";
     dot3.style.opacity = "100%";
+    dot4.style.opacity = "50%";
+      break;
+   case "row4":
+  row1.style.display = "none";
+  row2.style.display = "none";
+  row3.style.display = "none";
+  row4.style.display = "flex";
+  dot1.style.opacity = "50%";
+  dot2.style.opacity = "50%";
+  dot3.style.opacity = "50%";
+  dot4.style.opacity = "100%";
       break;
     default:
       console.log("no work");
   }
 };
+
+search("row1");
